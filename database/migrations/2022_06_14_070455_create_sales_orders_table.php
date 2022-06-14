6461488af8513474739e1de8202a7132d97c25f0
Henry Wijaya <henrywijaya92@gmail.com>
2022-06-14 14:34:04 +0700

    Init Setup

---
+++ database/migrations/2022_06_14_070455_create_sales_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales_orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_id');
+            $table->integer('qty');
+            $table->double('price_on_order');
+            $table->string('phone');
+            $table->string('image')->nullable();
+            $table->boolean('isCompleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales_orders');
+    }
+};
